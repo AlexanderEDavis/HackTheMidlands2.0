cf2c11ed1b57cd9d1ec296b318d8554be4f75611
Alexander Davis <AlexanderEDavis@users.noreply.github.com>
2017-09-23 19:57:04 +0100

    Changes to Template

--- template.php
+++ template.php
@@ -6,25 +6,10 @@
 
   <body>
     <nav>
-      <h1>project goes here</h1>
       <?php
         include 'navigationvisit.php'
       ?>
     </nav>
-
-    <section id="header">
-        <div id="header-content" style="width:100%; height: 60%; overflow:auto;">
-
-
-      </div>
-    </section>
-
-    <section id="content">
-      <div class="wrap" style="width:100%; overflow:auto;">
-
-      </div>
-
-    </section>
 
     <?php
       include 'footer.php'
