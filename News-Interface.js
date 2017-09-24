48e2a714364fb0e61b0290fb331a312a87194e02
jEdwardz <jamie.edwards5454@gmail.com>
2017-09-24 02:15:22 +0100

    Added getNewsSources to fetch an array of available news outlets

--- News-Interface.js
+++ News-Interface.js
@@ -2,13 +2,14 @@
 // var section = document.querySelector('section');
 
 //Define NewsAPI query here through URL params
-var reqURL = 'https://newsapi.org/v1/articles?source=techcrunch&apiKey=b348f8cb6e91416fa512d1d00cf5d98c';
-
+var articlesURL = 'https://newsapi.org/v1/articles?source=techcrunch&apiKey=b348f8cb6e91416fa512d1d00cf5d98c';
+var sourcesURL = 'https://newsapi.org/v1/sources'
 // Call to getJSON method takes above URL as argument
 // will not be needed when called from PHP
-getJSON(reqURL);
+// getArticles(articlesURL);
+getNewsSources(sourcesURL);
 
-function getJSON (requestURL) {
+function getArticles(requestURL) {
   var req = new XMLHttpRequest();
   req.open('GET', requestURL, true);
   req.responseType = 'json';
@@ -26,10 +27,35 @@ function unpackArticles(jsonObj) {
   for (var i = 0; i < results.length; i++) {
     var Article = {};
     Article["heading"] = results[i].title;
-    Article["url"] = results[i].url;
+    Article["URL"] = results[i].url;
     ArticlesArray.push(Article)
   }
   // console.log(ArticlesArray);
+}
+
+function getNewsSources(requestURL) {
+  var req = new XMLHttpRequest();
+  req.open('GET', requestURL, true);
+  req.responseType = 'json';
+  req.send();
+
+  req.onload = function () {
+    var results = req.response['sources'];
+    var SourceArray = [];
+    for (var i = 0; i < results.length; i++) {
+      var Source = {};
+      Source["id"] = results[i].id;
+      Source["name"] = results[i].name;
+      Source["description"] = results[i].description;
+      Source["URL"] = results[i].url;
+      Source["category"] = results[i].category;
+      Source["language"] = results[i].language;
+      Source["country"] = results[i].country;
+      Source["logoURL"] = results[i].urlsToLogos.medium;
+      SourceArray.push(Source);
+    }
+    console.log(SourceArray);
+  }
 }
 
 // function showArticles(jsonObj) {
