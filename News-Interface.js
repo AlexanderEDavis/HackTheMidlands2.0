085827afc3cfbd1cb84463cde748e0103c32aceb
jEdwardz <jamie.edwards5454@gmail.com>
2017-09-24 02:16:57 +0100

    Tidied up and made comments more concise

--- News-Interface.js
+++ News-Interface.js
@@ -1,13 +1,10 @@
-// var header = document.querySelector('header');
-// var section = document.querySelector('section');
-
-//Define NewsAPI query here through URL params
+//Define NewsArticles and NewsSources queries here through URL params
 var articlesURL = 'https://newsapi.org/v1/articles?source=techcrunch&apiKey=b348f8cb6e91416fa512d1d00cf5d98c';
 var sourcesURL = 'https://newsapi.org/v1/sources'
 // Call to getJSON method takes above URL as argument
 // will not be needed when called from PHP
 // getArticles(articlesURL);
-getNewsSources(sourcesURL);
+// getNewsSources(sourcesURL);
 
 function getArticles(requestURL) {
   var req = new XMLHttpRequest();
@@ -30,7 +27,6 @@ function unpackArticles(jsonObj) {
     Article["URL"] = results[i].url;
     ArticlesArray.push(Article)
   }
-  // console.log(ArticlesArray);
 }
 
 function getNewsSources(requestURL) {
@@ -54,30 +50,5 @@ function getNewsSources(requestURL) {
       Source["logoURL"] = results[i].urlsToLogos.medium;
       SourceArray.push(Source);
     }
-    console.log(SourceArray);
   }
 }
-
-// function showArticles(jsonObj) {
-//   var articles = jsonObj['articles'];
-//
-//   for (var i = 0; i < articles.length; i++) {
-//     var myArticle = document.createElement('article');
-//     var myH2 = document.createElement('h2');
-//     var myPara1 = document.createElement('p');
-//     var myPara2 = document.createElement('p');
-//     var myPara3 = document.createElement('p');
-//
-//     myH2.textContent = articles[i].title;
-//     myPara1.textContent = 'Written by: ' + articles[i].author;
-//     myPara2.textContent = articles[i].description;
-//     myPara3.textContent = 'Link to article: ' + articles[i].url;
-//
-//     myArticle.appendChild(myH2);
-//     myArticle.appendChild(myPara1);
-//     myArticle.appendChild(myPara2);
-//     myArticle.appendChild(myPara3);
-//
-//     section.appendChild(myArticle);
-//   }
-// }
