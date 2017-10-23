822bc740d13b7bf347bc8ec434e53bb9710108ed
Alexander Davis <AlexanderEDavis@users.noreply.github.com>
2017-10-23 23:59:58 +0100

    Footer Update

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
           <p>Site Developed By <a href="https://www.alexanderdavis.tech">Alexander Davis</a>, Lyall Lyall and <a href="http://www.jedwards.co.uk/">Jamie Edwards</a><br>All Rights Reserved</p>
         </div>
         <div class="copy-right" style="float: right;text-align: right;margin-right: 20px; margin-top:10px;">
-          <p style="margin-bottom: 0px;">© 2017 Alexander Davis, Lyall Lyall &amp; Jamie Edwards</p>
+          <p style="margin-bottom: 0px;">© 2017 Alexander Davis, Lyall Lyall &amp; James Edwards</p>
         </div>
     </footer>
 </section>
